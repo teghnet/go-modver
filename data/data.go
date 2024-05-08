b5af5b91c0234434f5de360dc4917034bd194b94
Paweł Zaremba <pawzar@gmail.com>
2024-05-08 15:09:24 +0200

    Version 0

---
+++ data/data.go
@@ -0,0 +1,8 @@
+package data
+
+import (
+	_ "embed"
+)
+
+//go:embed data.txt
+var Data string
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/teghnet/go-modver/data"
+)
+
+func main() {
+	fmt.Println("=== START ===")
+	fmt.Println(data.Data)
+	fmt.Println("=== STOP ===")
+}
